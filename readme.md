0a803959b860262e91140fab245a3eb5e7994601
ck2136 <chong.kim@ucdenver.edu>
2019-10-06 09:34:57 -0600

    code to transfer final simulation result from contianer to host

--- readme.md
+++ readme.md
@@ -3,7 +3,8 @@
 This repository contains terraform code to:
 
    1. Instantiate a digital ocean droplet
-   2. Install r-base in the apt repo
+   2. Install and run rocker/r-devel instance
+   3. Run PMMSKNN simulation script that utilizes the maximum allowabout CPU on a single droplet (32vCPU) for parallel processing
 
 The user still needs to input the commands
 
@@ -31,3 +32,14 @@ terraform plan -destroy -out=terraform.tfplan \
 
 terraform apply terraform.tfplan
 ```
+
+# Downloading the result of the simulation from the droplet
+
+After instantiating the droplet, the results of the simulation script will not be passed through STDOUT (for linux). Therefore One should set a script that would transfer the file from the droplet to your computer and then one could set a script to notify the user that the file has been produced so that the instance may be destroyed (to save cost).
+
+Below is the code to specify in the `www-1.tf` file:
+```bash
+"sudo scp sim1_BCCG_1000.RDS ck@ipaddress:/home/ck/Downloads/"
+```
+
+Currently there is no automatic procedure to destroy the instance. One could set up a hook/trigger to do so. If you hav eany ideas please let me know: [Email](mailto:chong.kim@ucdenver.edu)
--- www-1.tf
+++ www-1.tf
@@ -2,7 +2,8 @@ resource "digitalocean_droplet" "www-1" {
   image = "ubuntu-18-04-x64"
   name = "www-1"
   region = "nyc1"
-  size = "1gb"
+  # The size should be decided based on what the user is looking for here I'm using compute optimized
+  size = "c-32"
   private_networking = true
   ssh_keys = [
     "${var.ssh_fingerprint}"
@@ -19,16 +20,28 @@ resource "digitalocean_droplet" "www-1" {
   provisioner "remote-exec" {
     inline = [
       "export PATH=$PATH:/usr/bin",
-      # install R
-
-      ## Adds the CRAN GPG key, which is used to sign the R packages for security.
-      "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9",
-
-      ## Add apt repo for R
-      "sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'",
-
+      # install docker
       "sudo apt-get update",
-      "sudo apt-get install -y r-base"
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
+
+      # add Docker GPG key
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - ",
+      
+      # add apt-repository 
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
+                     
+      # Update the list of packages available to download... then install docker
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+
+      # Download docker image into VPS and Run the simulation
+      "sudo docker run -dti --name=dt1 ck2136/pmmsknn-test",
+
+      # Copy simulation results from container to VPS (do droplet)
+      "sudo docker cp dt1:/usr/local/src/myscripts/data/sim1_NO_1000.RDS . "
+
+      # Copy simulation results from VPS to original remote host
+      #"sudo scp sim1_NO_1000.RDS ck1@ipaddress:/home/ck/Downloads/"
     ]
   }
 }
